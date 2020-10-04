c37df949a29c31bd63484a3276a81f590cb6cf7a
Vanille-N <neven.villani@gmail.com>
2020-10-04 08:04:58 +0200

    rust partition interface

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,14 @@ mod qsort {
     mod internal {
         extern "C" {
             pub fn swap(tab: *mut i64, i: usize, j: usize);
+            pub fn partition(
+                tab: *mut i64,
+                lo: usize,
+                sm: *mut usize,
+                eq: *mut usize,
+                hi: usize,
+                pv: i64,
+            );
         }
     }
     pub fn swap(tab: &mut Vec<i64>, i: usize, j: usize) {
@@ -10,4 +18,25 @@ mod qsort {
             internal::swap(tab.as_mut_ptr() as *mut i64, i, j);
         }
     }
+    pub fn partition(
+        tab: &mut Vec<i64>,
+        lo: usize,
+        sm: &mut usize,
+        eq: &mut usize,
+        hi: usize,
+        pv: i64,
+    ) {
+        unsafe {
+            internal::partition(
+                tab.as_mut_ptr() as *mut i64,
+                lo,
+                sm as *mut usize,
+                eq as *mut usize,
+                hi,
+                pv,
+            );
+        }
+    }
+}
+
 }
